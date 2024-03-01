c4620e6524c7e9b6b995ce971448ebce56619f2b
mamimama2024 <mamimama2024@gmail.com>
2024-03-01 16:54:43 +0900

    ユーザーモデルの単体テスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,23 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Please use full-width characters.' } do
+    validates :nickname
+    validates :last_name
+    validates :first_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: 'Please use full-width katakana characters.' } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
   validates :birthday, presence: true
+  validate :password_complexity
 
+  private
+
+  def password_complexity
+    return if password.blank? || password =~ /^(?=.*[a-zA-Z])(?=.*\d).+$/
+
+    errors.add :password, 'must include both letters and numbers'
+  end
 end
