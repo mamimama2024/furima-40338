f80723cd93006282531287218d8007fbf835baca
mamimama2024 <mamimama2024@gmail.com>
2024-03-04 18:58:24 +0900

    商品情報をデータベースに保存

---
+++ app/models/scheduled_delivery.rb
@@ -0,0 +1,12 @@
+class ScheduledDelivery < ActiveHash::Base
+  self.data = [
+    { id: 1, status: '---' },
+    { id: 2, status: '1~2日で発送' },
+    { id: 3, status: '2~3日で発送' },
+    { id: 4, status: '4~7日で発送' }
+]
+
+  include ActiveHash::Associations
+  has_many :items
+
+end
