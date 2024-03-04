c5cc241cb36d94f83a5bb348f97a04fe557ec221
mamimama2024 <mamimama2024@gmail.com>
2024-03-04 16:56:55 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,6 +3,18 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(items_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def items_params
+    params.require(:items).permit(:name, :info, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price, :image ).merge(user_id: current_user.id)
   end
 
 end
