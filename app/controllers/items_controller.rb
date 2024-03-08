e2a54847a93a8c638b3f310185d1197a8b545823
mamimama2024 <mamimama2024@gmail.com>
2024-03-08 11:56:23 +0900

    before_actionの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: :new
   before_action :set_item, only: [:edit, :show]
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show, :update]
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
